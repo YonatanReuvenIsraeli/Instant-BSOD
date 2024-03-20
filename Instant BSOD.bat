5bee7ab399a4288c77285d096e845939fc35f62a
Yonatan Israeli <yonirisraeli@gmail.com>
2024-03-19 20:40:55 -0400

    Update Instant BSOD.bat

--- Instant BSOD.bat
+++ Instant BSOD.bat
@@ -4,31 +4,31 @@ title Instant BSOD
 echo Please run this batch file as an administrator.
 echo.
 echo READ DISCLAIMER ^-^-^> THIS IS FOR EDUCATIONAL PURPOSES ONLY! DO NOT USE THIS ON SOMEONE ELSES PC WITHOUT THEIR EXPLICIT PERMISSION! THIS WILL BLUE SCREEN OF DEATH THIS PC! CONTINUE AT YOUR OWN RISK! WE HOLD NO RESPONSIBILITY FOR PC DAMAGE, LOSS OF DATA, AND/OR ANYTHING ELSE, EVEN IF THE INSTRUCTIONS ARE WRONG, AND/OR MISSING, AND/OR THIS BATCH FILE DOES SOMETHING ELSE OTHER THAN INTENDED!
-goto :Disclaimer
+goto Disclaimer
 
 :Disclaimer
 echo.
 set /p Disclaimer="Do you agree to the Disclaimer? (Yes/No) "
-if /i "%Disclaimer%"=="Yes" goto :svchost
-if /i "%Disclaimer%"=="No" goto :Close
+if /i "%Disclaimer%"=="Yes" goto svchost
+if /i "%Disclaimer%"=="No" goto Close
 echo Invalid Syntax!
-goto :Disclaimer
+goto Disclaimer
 
 :svchost
 echo.
 set /p svchost="This will end the system process svchost.exe and will Blue Screen of Death this PC. Are you okay with that? (Yes/No) "
-if /i "%svchost%"=="Yes" goto :Warning
-if /i "%svchost%"=="No" goto :Close
+if /i "%svchost%"=="Yes" goto Warning
+if /i "%svchost%"=="No" goto Close
 echo Invalid Syntax!
-goto :svchost
+goto svchost
 
 :Warning
 echo.
 set /p Warning="READ WARNING --> THERE IS NO GOING BACK AFTER THIS! THIS IS YOUR LAST CHANCE TO STOP! THIS WILL BLUE SCREEN OF DEATH THIS COMPUTER! ARE YOU SURE YOU WANT TO CONTINUE? (Yes/No) "
-if /i "%Warning%"=="Yes" goto :BSOD
-if /i "%Warning%"=="No" goto :Close
+if /i "%Warning%"=="Yes" goto BSOD
+if /i "%Warning%"=="No" goto Close
 echo Invalid Syntax!
-goto :Warning
+goto Warning
 
 :BSOD
 endlocal
