635a3bc2492877fc990bf15edc01bea70338bcb6
Yonatan Israeli <yonirisraeli@gmail.com>
2024-08-14 15:57:52 -0400

    Update Instant BSOD.bat

--- Instant BSOD.bat
+++ Instant BSOD.bat
@@ -2,54 +2,53 @@
 setlocal
 title Instant BSOD
 echo Program Name: Instant BSOD
-echo Version: 2.0.5
+echo Version: 2.0.6
 echo Developer: @YonatanReuvenIsraeli
 echo Website: https://www.yonatanreuvenisraeli.dev
 echo License: GNU General Public License v3.0
 net session > nul 2>&1
-if not "%errorlevel%"=="0" goto NotAdministrator
-goto Disclaimer
+if not "%errorlevel%"=="0" goto "NotAdministrator"
+goto "Disclaimer"
 
-:NotAdministrator
+:"NotAdministrator"
 echo.
 echo Please run this batch file as an administrator. Press any key to close this batch file.
 pause > nul 2>&1
-goto Close
+goto "Close"
 
-:Disclaimer
+:"Disclaimer"
 echo.
 echo READ DISCLAIMER ^-^-^> THIS IS FOR EDUCATIONAL PURPOSES ONLY! DO NOT USE THIS ON SOMEONE ELSES PC WITHOUT THEIR EXPLICIT PERMISSION! THIS WILL BLUE SCREEN OF DEATH THIS PC! CONTINUE AT YOUR OWN RISK! WE HOLD NO RESPONSIBILITY FOR PC DAMAGE, LOSS OF DATA, AND/OR ANYTHING ELSE, EVEN IF THE INSTRUCTIONS ARE WRONG, AND/OR MISSING, AND/OR THIS BATCH FILE DOES SOMETHING ELSE OTHER THAN INTENDED!
 echo.
 set Disclaimer=
 set /p Disclaimer="Do you agree to the Disclaimer? (Yes/No) "
-if /i "%Disclaimer%"=="Yes" goto svchost
-if /i "%Disclaimer%"=="No" goto Close
+if /i "%Disclaimer%"=="Yes" goto "svchost"
+if /i "%Disclaimer%"=="No" goto "Close"
 echo Invalid Syntax!
-goto Disclaimer
+goto "Disclaimer"
 
-:svchost
+:"svchost"
 echo.
 set svchost=
 set /p svchost="This will end the system process svchost.exe and will Blue Screen of Death this PC. Are you okay with that? (Yes/No) "
-if /i "%svchost%"=="Yes" goto Warning
-if /i "%svchost%"=="No" goto Close
+if /i "%svchost%"=="Yes" goto "Warning"
+if /i "%svchost%"=="No" goto "Close"
 echo Invalid syntax!
-goto svchost
+goto "svchost"
 
-:Warning
+:"Warning"
 echo.
 set Warning=
 set /p Warning="READ WARNING --> THERE IS NO GOING BACK AFTER THIS! THIS IS YOUR LAST CHANCE TO STOP! THIS WILL BLUE SCREEN OF DEATH THIS COMPUTER! ARE YOU SURE YOU WANT TO CONTINUE? (Yes/No) "
-if /i "%Warning%"=="Yes" goto BSOD
-if /i "%Warning%"=="No" goto Close
+if /i "%Warning%"=="Yes" goto "BSOD"
+if /i "%Warning%"=="No" goto "Close"
 echo Invalid syntax!
-goto Warning
+goto "Warning"
 
-:BSOD
+:"BSOD"
 endlocal
 taskkill /f /im svchost.exe
-exit
 
-:Close
+:"Close"
 endlocal
 exit
