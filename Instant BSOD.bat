f269eba4e03da7856cf2399a25de236fecb711f2
Yonatan Israeli <yonirisraeli@gmail.com>
2024-12-19 18:50:42 -0500

    Update Instant BSOD.bat

--- Instant BSOD.bat
+++ Instant BSOD.bat
@@ -2,10 +2,11 @@
 setlocal
 title Instant BSOD
 echo Program Name: Instant BSOD
-echo Version: 2.0.7
-echo Developer: @YonatanReuvenIsraeli
-echo Website: https://www.yonatanreuvenisraeli.dev
+echo Version: 2.0.8
 echo License: GNU General Public License v3.0
+echo Developer: @YonatanReuvenIsraeli
+echo GitHub: https://github.com/YonatanReuvenIsraeli
+echo Sponsor: https://github.com/sponsors/YonatanReuvenIsraeli 
 net session > nul 2>&1
 if not "%errorlevel%"=="0" goto "NotAdministrator"
 goto "Disclaimer"
